3e88b85c7aefd5a18f0e096d1f8c4c0a24977476
Miguel <miguelsanglard@gmail.com>
2024-03-12 11:55:09 -0300

    calculo distancias quando eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -2,34 +2,45 @@ export default class Slide {
 constructor(slide, wrapper){
   this.slide = document.querySelector(slide)
   this.wrapper = document.querySelector(wrapper)
+  this.dist = {
+    finalPosition: 0, startX: 0, movement: 0}
 }
+  moveSlide(distX){
+    this.dist.movePositon = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+  updatePosition(clientX){
+    this.dist.movement = (this.dist.startX - clientX) * 1.5
+    return this.dist.finalPosition - this.dist.movement;
+  }
 
   onStart(event){
     event.preventDefault();
-    console.log('mousedown')
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
-
   }
+
   onMove(event){
-    console.log('moveu')
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition)
   }
-  onEnd(event){
-    console.log('acabou')
-    this.wrapper.removeEventListener('mousemove', this.onMove);
 
+  onEnd(event){
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePositon;
   }
+
   addSlideEvents(){
     this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
-
   }
+
   bindEvents(){
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
-
-
   }
+
   init(){
     this.bindEvents()
     this.addSlideEvents()
